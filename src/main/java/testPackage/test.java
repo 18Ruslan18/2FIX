6f754ecd01c0ea6fc22fb70b93cff18f2531e0e3
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-06 00:02:50 +0300

    commit test java

--- src/main/java/testPackage/test.java
+++ src/main/java/testPackage/test.java
@@ -1,4 +1,28 @@
 package testPackage;
 
 public class test {
+    private String name;
+    private String tem;
+
+    public test(String name, String tem) {
+        this.name = name;
+        this.tem = tem;
+    }
+    public test(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTem() {
+        return tem;
+    }
+
+    public void setTem(String tem) {
+        this.tem = tem;
+    }
 }
